9284bbc7724e9eb534fcb101841d2a9d5c02f916
usrrt <gkenfktm@gmail.com>
2022-08-25 16:16:21 +0900

    타이틀 씬 구현

---
+++ My project/Assets/Scripts/MenuScripts/TitleScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Info()
+    {
+        SceneManager.LoadScene("InfoScene");
+    }
+}
